ae8fb752c13f8dd8c71b27add42a24df24dbf56e
Ganesh Gagare <ganesh.gagare.19@gamil.com>
2025-10-20 00:58:17 +0530

    Day 1-2 Updated

---
+++ Practice Questions/Day2/PalindromeNumber.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class PalindromeNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number: ");
+        int n = sc.nextInt();
+        int original = n;
+        int reversed = 0;
+
+        while (n != 0) {
+            reversed = reversed * 10 + n % 10;
+            n /= 10;
+        }
+
+        if (original == reversed)
+            System.out.println("Palindrome: True");
+        else
+            System.out.println("Palindrome: False");
+
+        sc.close();
+    }
+}
